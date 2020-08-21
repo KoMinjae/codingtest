662b47ee14799451da1ef351dfd3ba25cee7c9a2
KoMinjae <alswovv@naver.com>
2020-08-21 14:54:47 +0900

    2020.08.21(재 풀이)

--- 네트워크.py
+++ 네트워크.py
@@ -14,3 +14,48 @@ def solution(n, computers):
         answer += 1
     return answer
 solution(3,	[[1, 1, 0], [1, 1, 0], [0, 0, 1]])
+"""
+from collections import defaultdict
+def solution(n, computers):
+    graph=dict()
+    lens = len(computers)
+    #
+    if lens ==1:
+        return 1
+    for i in range(lens):
+        graph[i] = list()
+    #
+    
+    for i in range(len(computers)):
+        for j in range(len(computers)):
+            if i != j:
+                if computers[i][j] == 1:
+                    graph[i].append(j)
+    visited=list()
+    nodes=list()
+    for i in range(lens):
+        nodes.append(i)
+    stack=list()
+    stack.append(0)
+    visited.append(0)
+    nodes.remove(0)
+    count=0
+    while len(visited) != lens:
+        if not stack:
+            stack.append(nodes[0])
+            visited.append(nodes[0])
+            nodes.remove(nodes[0])
+        while stack:
+            node = stack.pop(0)
+            if node in graph:
+                for i in graph[node]:
+                    if i not in visited:
+                        stack.append(i)
+                        visited.append(i)
+                        nodes.remove(i)
+        count+=1
+    return count
+    
+
+print(solution(3,[1]))
+"""
